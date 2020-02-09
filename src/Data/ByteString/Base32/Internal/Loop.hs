b5e4797b12a9fe8fdb8f8b120b0318e66451b891
Emily Pillmore <emily@kadena.io>
2020-02-08 23:32:19 -0500

    loop fixes

--- src/Data/ByteString/Base32/Internal/Loop.hs
+++ src/Data/ByteString/Base32/Internal/Loop.hs
@@ -37,7 +37,6 @@ innerLoop !lut !dptr !sptr !end finish = go dptr sptr
 #else
         !t <- byteSwap32 <$> peek @Word32 (castPtr src)
 #endif
-
         !u <- w32 <$> peek (plusPtr src 4)
 
         let !a = (unsafeShiftR t 27)
@@ -61,7 +60,7 @@ innerLoop !lut !dptr !sptr !end finish = go dptr sptr
 
 innerLoopNopad
     :: Ptr Word8
-    -> Ptr Word64
+    -> Ptr Word8
     -> Ptr Word8
     -> Ptr Word8
     -> (Ptr Word8 -> Ptr Word8 -> Int -> IO ByteString)
@@ -80,13 +79,13 @@ innerLoopNopad (Ptr !lut) !dptr !sptr !end finish = go dptr sptr 0
 #endif
         !u <- w32 <$> peek (plusPtr src 4)
 
-        !aa <- lix dst 0 (unsafeShiftR t 27)
-        !bb <- lix dst 1 (unsafeShiftR t 22)
-        !cc <- lix dst 2 (unsafeShiftR t 17)
-        !dd <- lix dst 3 (unsafeShiftR t 12)
-        !ee <- lix dst 4 (unsafeShiftR t 7)
-        !ff <- lix dst 5 (unsafeShiftR t 2)
-        !gg <- lix dst 6 ((unsafeShiftL t 3) .|. (unsafeShiftR u 5))
-        !hh <- lix dst 7 u
+        lix dst 0 (unsafeShiftR t 27)
+        lix dst 1 (unsafeShiftR t 22)
+        lix dst 2 (unsafeShiftR t 17)
+        lix dst 3 (unsafeShiftR t 12)
+        lix dst 4 (unsafeShiftR t 7)
+        lix dst 5 (unsafeShiftR t 2)
+        lix dst 6 ((unsafeShiftL t 3) .|. (unsafeShiftR u 5))
+        lix dst 7 u
 
         go (plusPtr dst 8) (plusPtr src 5) (n + 8)
