f6ba5a760dad4036ace8a4bd96be72f7331b6edd
Emily Pillmore <emily@kadena.io>
2020-02-16 03:32:55 -0500

    fix inline pragma

--- src/Data/Text/Encoding/Base32.hs
+++ src/Data/Text/Encoding/Base32.hs
@@ -50,7 +50,7 @@ decodeBase32 = fmap T.decodeUtf8 . B32.decodeBase32 . T.encodeUtf8
 --
 encodeBase32Unpadded :: Text -> Text
 encodeBase32Unpadded = B32.encodeBase32 . T.encodeUtf8
-{-# INLINE encodeBase32 #-}
+{-# INLINE encodeBase32Unpadded #-}
 
 -- | Decode an arbitrarily padded Base32-encoded 'Text'
 --
