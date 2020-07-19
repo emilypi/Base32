af95394bcce285f56459918e435f07f005ce2035
Emily Pillmore <emilypi@cohomolo.gy>
2020-07-19 14:30:04 -0400

    fix for Joachim's issue

--- src/Data/ByteString/Base32/Internal/Head.hs
+++ src/Data/ByteString/Base32/Internal/Head.hs
@@ -9,10 +9,10 @@ module Data.ByteString.Base32.Internal.Head
 ) where
 
 
-import qualified Data.ByteString as BS
 import Data.ByteString.Internal
 import Data.ByteString.Base32.Internal.Loop
 import Data.ByteString.Base32.Internal.Tail
+import Data.ByteString.Base32.Internal.Utils
 import Data.Text (Text)
 
 import Foreign.Ptr
@@ -28,64 +28,49 @@ import System.IO.Unsafe
 -- | Head of the base32 encoding loop - marshal data, assemble loops
 --
 encodeBase32_ :: Addr# -> ByteString -> ByteString
-encodeBase32_ !lut (PS !sfp !soff !slen) =
-    unsafeCreate dlen $ \dptr ->
-      withForeignPtr sfp $ \sptr -> do
-        let !end = plusPtr sptr (soff + slen)
-        innerLoop
-          lut
-          (castPtr dptr)
-          (plusPtr sptr soff)
-          end
-          (loopTail lut end)
+encodeBase32_ !lut (PS !sfp !o !l) =
+    unsafeDupablePerformIO $ do
+      dfp <- mallocPlainForeignPtrBytes dlen
+      withForeignPtr dfp $ \dptr ->
+        withForeignPtr sfp $ \sptr -> do
+          let !end = plusPtr sptr (l + o)
+          innerLoop
+            lut
+            (castPtr dptr)
+            (plusPtr sptr o)
+            end
+            (loopTail lut dfp dptr end)
   where
-    !dlen = 8 * ((slen + 4) `div` 5)
+    !q = (l * 8) `quot` 5
+    !r = (l * 8) `rem` 5
+    !dlen = padCeilN 8 (q + if r == 0 then 0 else 1)
 
 -- | Head of the unpadded base32 encoding loop - marshal data, assemble loops
 --
 encodeBase32NoPad_ :: Addr# -> ByteString -> ByteString
-encodeBase32NoPad_ !lut (PS !sfp !soff !slen)
-    = unsafeDupablePerformIO $ do
+encodeBase32NoPad_ !lut (PS !sfp !o !l) =
+    unsafeDupablePerformIO $ do
       !dfp <- mallocPlainForeignPtrBytes dlen
       withForeignPtr dfp $ \dptr ->
         withForeignPtr sfp $ \sptr -> do
-          let !end = plusPtr sptr (soff + slen)
-          innerLoopNoPad
-            lut
+          let !end = plusPtr sptr (l + o)
+          innerLoop lut
             (castPtr dptr)
-            (plusPtr sptr soff)
+            (plusPtr sptr o)
             end
-            (loopTailNoPad lut dfp end)
+            (loopTailNoPad lut dfp dptr end)
   where
-    !dlen = 8 * ((slen + 4) `div` 5)
+    !q = (l * 8) `quot` 5
+    !r = (l * 8) `rem` 5
+    !dlen = padCeilN 8 (q + if r == 0 then 0 else 1)
 
 -- | Head of the base32 decoding loop - marshal data, assemble loops
 --
-decodeBase32_ :: Bool -> ForeignPtr Word8 -> ByteString -> Either Text ByteString
-decodeBase32_ !pad !alphabet bs@(PS _ _ !l)
-    | l == 0 = Right ""
-    | r /= 0, pad =
-      if
-        | r == 2 -> go (BS.append bs (BS.replicate 6 0x3d))
-        | r == 4 -> go (BS.append bs (BS.replicate 4 0x3d))
-        | r == 5 -> go (BS.append bs (BS.replicate 3 0x3d))
-        | r == 7 -> go (BS.append bs (BS.replicate 1 0x3d))
-        | otherwise -> Left "invalid bytestring size"
-    | r /= 0, not pad = Left "invalid padding"
-    | otherwise = go bs
-  where
-    (!q, !r) = l `divMod` 8
-    !dlen = q * 8
-
-    go (PS !sfp !soff !slen) = unsafeDupablePerformIO $ do
-      !dfp <- mallocPlainForeignPtrBytes dlen
-      withForeignPtr dfp $ \dptr ->
-        withForeignPtr alphabet $ \(Ptr lut) ->
-        withForeignPtr sfp $ \sptr -> do
-          let !end = plusPtr sptr (soff + slen)
-          decodeLoop
-            lut
-            (castPtr dptr)
-            (plusPtr sptr soff)
-            end
-            (decodeTail lut dfp end)
+decodeBase32_ :: Int -> ForeignPtr Word8 -> ByteString -> IO (Either Text ByteString)
+decodeBase32_ !dlen !dtfp (PS !sfp !soff !slen) =
+    withForeignPtr dtfp $ \(Ptr dtable) ->
+    withForeignPtr sfp $ \sptr -> do
+      dfp <- mallocPlainForeignPtrBytes dlen
+      withForeignPtr dfp $ \dptr -> do
+        let !end = plusPtr sptr (soff + slen)
+        decodeLoop dtable dptr (plusPtr sptr soff) end (decodeTail dtable dfp end)
--- src/Data/ByteString/Base32/Internal/Loop.hs
+++ src/Data/ByteString/Base32/Internal/Loop.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE TypeApplications #-}
 module Data.ByteString.Base32.Internal.Loop
 ( innerLoop
-, innerLoopNoPad
 , decodeLoop
 ) where
 
@@ -29,8 +28,8 @@ innerLoop
     -> Ptr Word64
     -> Ptr Word8
     -> Ptr Word8
-    -> (Ptr Word8 -> Ptr Word8 -> IO ())
-    -> IO ()
+    -> (Ptr Word8 -> Ptr Word8 -> IO ByteString)
+    -> IO ByteString
 innerLoop !lut !dptr !sptr !end finish = go dptr sptr
   where
     lix a = return $ w64 (aix (fromIntegral a .&. 0x1f) lut)
@@ -65,53 +64,8 @@ innerLoop !lut !dptr !sptr !end finish = go dptr sptr
           .|. (unsafeShiftL h 56)
 
         poke dst w
-
         go (plusPtr dst 8) (plusPtr src 5)
 {-# INLINE innerLoop #-}
-
-innerLoopNoPad
-    :: Addr#
-    -> Ptr Word64
-    -> Ptr Word8
-    -> Ptr Word8
-    -> (Ptr Word8 -> Ptr Word8 -> Int -> IO ByteString)
-    -> IO ByteString
-innerLoopNoPad !lut !dptr !sptr !end finish = go dptr sptr 0
-  where
-    lix a = return $ w64 (aix (fromIntegral a .&. 0x1f) lut)
-
-    go !dst !src !n
-      | plusPtr src 4 >= end = finish (castPtr dptr) src n
-      | otherwise = do
-#ifdef WORDS_BIGENDIAN
-        !t <- peek @Word32 (castPtr src)
-#else
-        !t <- byteSwap32 <$> peek @Word32 (castPtr src)
-#endif
-        !u <- w32 <$> peek (plusPtr src 4)
-
-        !a <- lix (unsafeShiftR t 27)
-        !b <- lix (unsafeShiftR t 22)
-        !c <- lix (unsafeShiftR t 17)
-        !d <- lix (unsafeShiftR t 12)
-        !e <- lix (unsafeShiftR t 7)
-        !f <- lix (unsafeShiftR t 2)
-        !g <- lix ((unsafeShiftL t 3) .|. (unsafeShiftR u 5))
-        !h <- lix u
-
-        !w <- return $ a
-          .|. (unsafeShiftL b 8)
-          .|. (unsafeShiftL c 16)
-          .|. (unsafeShiftL d 24)
-          .|. (unsafeShiftL e 32)
-          .|. (unsafeShiftL f 40)
-          .|. (unsafeShiftL g 48)
-          .|. (unsafeShiftL h 56)
-
-        poke dst w
-
-        go (plusPtr dst 8) (plusPtr src 5) (n + 8)
-{-# INLINE innerLoopNoPad #-}
 
 -- ------------------------------------------------------------------------ --
 -- Decoding loops
