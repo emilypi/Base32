b02886217a16a7118f1e899a0ba71897fe5f91a5
Emily Pillmore <emilypi@cohomolo.gy>
2020-07-23 11:33:54 -0400

    update nix

--- default.nix
+++ default.nix
@@ -4,22 +4,26 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, bytestring, criterion, deepseq, memory
-      , random-bytestring, stdenv, tasty, tasty-hunit, text
+  f = { mkDerivation, base, bytestring, criterion, deepseq
+      , ghc-byteorder, memory, QuickCheck, random-bytestring, stdenv
+      , tasty, tasty-hunit, tasty-quickcheck, text, text-short
       }:
       mkDerivation {
         pname = "base32";
-        version = "0.1.0.0";
+        version = "0.2.0.0";
         src = ./.;
-        libraryHaskellDepends = [ base bytestring text ];
+        libraryHaskellDepends = [
+          base bytestring deepseq ghc-byteorder text text-short
+        ];
         testHaskellDepends = [
-          base memory random-bytestring tasty tasty-hunit text
+          base bytestring memory QuickCheck random-bytestring tasty
+          tasty-hunit tasty-quickcheck text text-short
         ];
         benchmarkHaskellDepends = [
           base bytestring criterion deepseq memory random-bytestring text
         ];
         homepage = "https://github.com/emilypi/base32";
-        description = "RFC 4648-compliant Base32 encodings/decodings";
+        description = "Fast RFC 4648-compliant Base32 encoding";
         license = stdenv.lib.licenses.bsd3;
       };
 
